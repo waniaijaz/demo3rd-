3f2f946c3dfb3d370c5a898a11d50e53c52ce1f8
Wani <aijaz.18jics204@jietjodhpur.ac.in>
2021-06-03 12:11:14 +0530

    demo practice third

---
+++ main.js
@@ -0,0 +1,34 @@
+// console.log("helloworld");
+// var x=10;
+// var y=20;
+// console.log(x+y);
+// alert(5+6);
+// var person="aijaz";
+// var person1="ahmad";
+// console.log(person+"   "+person1);
+
+// function a(){
+//     var x=10;
+//     console.log(x);
+ 
+// }
+// a();
+let hello="hello everyone";
+ hello="greeatings";
+{
+   let hello="hello world"
+    console.log(hello)
+}
+for(let i=0;i<=50;i++){
+    console.log( i +"*2="+i*2)
+}
+var ist=10;
+var second =20;
+second +=ist;
+console.log(ist+second);
+console.log(ist-second);
+console.log(ist/second);
+console.log(ist * second);
+console.log(ist ** second);
+console.log(second % ist);
+console.log(typeof ist);
